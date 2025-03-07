452c7faa6be1197e98f0d61c2f095359b16c3b40
Inseong-dev <73svnptre@gmail.com>
2025-03-07 13:46:30 +0000

    FEAT: 기능 추가

--- myapp/lib/main.dart
+++ myapp/lib/main.dart
@@ -1,38 +1,14 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp( MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  Widget textSection1 = Text("hello1"); 
-  Widget textSection2 = Text("hello2"); 
-  Widget textSection3 = Text("hello3"); 
-  Widget textSection4 = Text("hello4"); 
-  List<Widget> item1 = [
-                SizedBox(
-              height: 200,
-              width: 200,
-              child: Text("a"),
-            ),
-            SizedBox(
-              height: 200,
-              width: 200,
-              child: Text("b"),
-            ),
-            SizedBox(
-              height: 200,
-              width: 200,
-              child: Text("c"),
-            ),
-            SizedBox(
-              height: 200,
-              width: 200,
-              child: Text("d"),
-            ),
-            
-  ];
-   MyApp({super.key});
+  MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -49,30 +25,45 @@ class Mylist extends StatefulWidget{
 }
 
 class _Mylist extends State<Mylist>{
-  List<Widget> item1 = [
-                SizedBox(
-              height: 200,
-              width: 200,
-              child: Text("a"),
+  List<String> item1 = [];
+  TextEditingController inputController = TextEditingController();
+  String inputText = "";
+  void plusitemlist() async{
+    await showDialog(
+      context: context,
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text(
+            "ADD TO-DO-LIST",
+            style: TextStyle(
+              fontWeight: FontWeight.w800,
+              fontSize: 20,
+              color: Colors.blue,
             ),
-            SizedBox(
-              height: 200,
-              width: 200,
-              child: Text("b"),
-            ),
-            SizedBox(
-              height: 200,
-              width: 200,
-              child: Text("c"),
-            ),
-            SizedBox(
-              height: 200,
-              width: 200,
-              child: Text("d"),
-            ),
-            
-  ];
-
+          ),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(
+                controller: inputController,
+              ),
+            ],
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text("Confirm"),
+              onPressed: (){
+                inputController.text.replaceAll(" ","") == "" ?  () : Navigator.pop(context);
+              },
+            )
+          ]
+        ); 
+      }
+    );
+    setState((){
+       item1.add(inputController.text);
+    });
+}
   @override
   Widget build(BuildContext context){
     return Scaffold(
@@ -88,14 +79,77 @@ class _Mylist extends State<Mylist>{
         body: ListView.builder(
           itemCount: item1.length,
           itemBuilder: (BuildContext context, int index){
-            
+            return  TodoItemList(itemText: item1[index], index: index + 1, devicesize: MediaQuery.of(context).size);
           },
         ),
         floatingActionButton: FloatingActionButton(
-          onPressed: () => {},
+          onPressed:plusitemlist,
           child: const Icon(Icons.add),
           ),
     );
   }
 }
+
+
+class TodoItemList extends StatefulWidget{
+  final devicesize;
+  final itemText;
+  final index;
+  
+  TodoItemList({
+    super.key,
+    required this.itemText,
+    required this.index,
+    required this.devicesize,
+  });
+
+  @override
+  State<TodoItemList> createState() => _TodoItemList();
+}
+class _TodoItemList extends State<TodoItemList>{
+ 
+  bool activationFlag = true;
+  Icon boxIcon = Icon(Icons.check_box_outline_blank);
+  void activationFlagChange(){
+    setState(() {
+      activationFlag = !activationFlag;
+      boxIcon = activationFlag? Icon(Icons.check_box_outline_blank) : Icon(Icons.check_box);
+    });
+    
+  }
+  @override
+  Widget build(BuildContext context){
+    return SizedBox(
+    height: widget.devicesize.height * 0.1,
+    child: Row(
+      children: [
+              Row(
+                children: [
+                  Text("${widget.index} : "),
+                  Text(
+                    widget.itemText, 
+                    style: TextStyle(
+                      decoration: activationFlag? TextDecoration.none : TextDecoration.lineThrough,
+                      ),
+                    ),
+                ],
+              ),
+              Row(
+                children: [
+                  IconButton(
+                  onPressed:activationFlagChange,
+                  icon: boxIcon,
+                  ),
+                  IconButton(
+                  onPressed:() {},
+                  icon: Icon(Icons.delete),
+                  )
+                ],
+              )
+            ]
+          )
+        );
+      }
+    }
+
 
